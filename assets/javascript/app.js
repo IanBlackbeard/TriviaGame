e4d564d2cb1c1a1a32ce8c1f63ef041fbc2ae224
Ian Nowicki <iannowicki@Ians-MacBook-Pro.local>
2018-01-27 22:40:24 -0700

    built most of the logic in the order of display. Still need to wire the user guess into the counter.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,38 @@
-//Start page with button to start the game
-	//
-
-//Opens to new page with timer at the top
-
-//Questions all listed in a row
-
-//Done button on bottom to trigger next page wit totals
-
-//4-5 option mutiple choices under the Q in form of radio button 
-
-//If time runs out new, page shows with correct answers, incorrect answers, unanswered all listed respectively
-  // wether or not all or answered or not.
 $(document).ready(function(){
-	$("#start-button").click(function(){
 
-		var number = 60;
+	$(".question").hide();
+	$(".answers").hide();
+	$("#submit").hide();
+	$("#restart").hide();
+	$("#correct").hide();
+	$("#wrong").hide();
+
+	$("#start-button").click(function(){
 		alert("Let's Go!!!")
+
+		var number = 4;
+		var correct = 0;
+		var wrong = 0;
+		var q1 = false;
+
+		$(".question").hide();
+		$(".answer").hide();
 
 		$('#start-button').on('click', start)
 		$('#submit').on('click', finish)
 		$('#restart').on('click', restart)
 
+		//Called when user clicks start
 		function start(){
 			counter = setInterval(timer, 1000);
-			showMe(".questions");
-			showMe(".answers");
-			showMe("#submit");
-			hideMe("#start-button");
-			hideMe(".rules");
+			$("#start-button").hide();
+			$(".rules").hide();
+			$("#restart").hide();
+			$(".question").show();
+			$(".answers").show();
+			$("#submit").show();	
 		}
+		//Called when user clicks start
 		function timer(){
 			number--
 			$("#show-number").html("<h2>" + number + "</h2>");
@@ -37,17 +41,54 @@ $(document).ready(function(){
 				stop();
 			}
 		}
+
+		//Called when timer runs out
 		function stop(){
 			clearInterval(counter);
-
+			// $(".results").show();
+			$("#show-number").hide();
+			$(".question").hide();
+			$(".answers").hide();
+			$("#submit").hide();
+			//
+			$("#restart").show();
+			$("#correct").show();
+			$("#wrong").show();
 		}
+
+		//Called when user clicks <submit>
 		function finish(){
 			clearInterval(counter);
+			// number = 0;
+			$("#show-number").hide();
+			$(".question").hide();
+			$(".answers").hide();
+			$("#submit").hide();
+			//
+			$("#restart").show();
+			$("#correct").show();
+			$("#wrong").show();
 		}
-		function restart(){
 
+		//Called when button id is clicked. button shows up after time runs out 
+		 //or when user clicks submit
+		function restart(){
+			$("#show-number").show();
+			$("#correct").hide();
+			$("#wrong").hide();
+			number =4;
+			start();
 		}
 
 		start();
 	});
 });
+
+//Start page with button to start the game
+//Opens to new page with timer at the top
+//Questions all listed in a row
+
+//Done button on bottom to trigger next page with totals
+//4-5 option mutiple choices under the Q in form of radio button 
+//If time runs out new, page shows with correct answers, incorrect answers, unanswered all listed respectively
+  // wether or not all or answered or not.
